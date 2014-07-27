4046b4408351b1efa1c3d1dd7bf8ce1ea67311c5
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-07-26 19:59:50 -0500

    fix build
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -377,7 +377,7 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
 extern struct cpufreq_governor cpufreq_gov_xperience;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_xperienc
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_xperience)
 #endif
 
 
