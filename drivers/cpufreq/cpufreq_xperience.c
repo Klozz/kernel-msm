7841b1e89f275c17eb9b8da14c20c3f53fbcee94
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-09-09 04:23:16 -0500

    XPerience: Make more faster scalling to down freq
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/cpufreq/cpufreq_xperience.c
+++ drivers/cpufreq/cpufreq_xperience.c
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * Autor: Carlos Jesús B.C (Aka Klozz or TeamMEX@XDA)
- * Based on xperiencev2
+ * Based on Smartassv2
  * 
  */
 
@@ -69,7 +69,7 @@ static unsigned int ramp_down_step;
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 50
+#define DEFAULT_MAX_CPU_LOAD 55
 static unsigned long max_cpu_load;
 
 /*
@@ -82,14 +82,14 @@ static unsigned long min_cpu_load;
  * The minimum amount of time to spend at a frequency before we can ramp up.
  * Notice we ignore this when we are below the ideal frequency.
  */
-#define DEFAULT_UP_RATE_US 48000;
+#define DEFAULT_UP_RATE_US 38000;
 static unsigned long up_rate_us;
 
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  * Notice we ignore this when we are above the ideal frequency.
  */
-#define DEFAULT_DOWN_RATE_US 49000;
+#define DEFAULT_DOWN_RATE_US 39000;
 static unsigned long down_rate_us;
 
 /*
